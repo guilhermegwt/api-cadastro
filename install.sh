bf257b16b480c2bdd0ce174123b65b023a4288be
guilhermegwt <guilhermegwt@hotmail.com>
2022-10-14 14:40:06 -0300

    config: modificando o script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-sudo add-apt-repository ppa:deadsnakes/ppa;
-sudo apt update;
-sudo apt-get install python3.10;
-sudo apt install python3-pip;
-sudo apt install python3-virtualenv;
+sudo add-apt-repository ppa:deadsnakes/ppa -y;
+sudo apt update -y;
+sudo apt-get install python3.10 -y;
+sudo apt install python3-pip -y;
+sudo apt install python3-virtualenv -y;
 virtualenv -p python3.10 venv;
 source venv/bin/activate;
 pip install -r requirements.txt;
