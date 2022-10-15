b59fab46a122d93c005995e7396add33322dc898
guilhermegwt <guilhermegwt@hotmail.com>
2022-10-15 14:27:54 -0300

    config: mudança no script de instalação

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt update -y;
 sudo apt-get install python3.10 -y;
 sudo apt install python3-pip -y;
 sudo apt install python3-virtualenv -y;
-virtualenv -p python3.10 venv;
+virtualenv -p python3 venv;
 source venv/bin/activate;
 pip install -r requirements.txt;
 python manage.py makemigrations cadastro;
